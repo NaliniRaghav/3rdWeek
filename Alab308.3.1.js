cd19a8ccced06c7e9545dd755b72892102eeba9c
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-08-09 19:02:25 -0400

    first two problems

--- Alab308.3.1.js
+++ Alab308.3.1.js
@@ -1,30 +1,54 @@
-let Div3 = 0
-let Div5 = 0
+let Div3 = 0;
+let Div5 = 0;
+let i = 0;
+
+for (i = 1; i < 100; i++) {
+  Div3 = i % 3;
+  Div5 = i % 5;
+
+  if (!(Div3 == 0 || Div5 == 0)) {
+    console.log(i);
+  } else {
+    if (Div3 == 0 && Div5 == 0) console.log(i + "  Fuzz Buzz");
+    else {
+      if (Div3 == 0) console.log(i + "  Fuzz ");
+      else if (Div5 == 0) console.log(i + "  Buzz ");
+    }
+  }
+}
+
+let input = prompt("Enter a number:");
+
+let n = Number(input);
+let P =0;
+let currentNumberIsPrime = true;
+let foundPrime = false;
 
 
+for (i=n+1; (foundPrime==false); i++) 
   
-for( i=1; i<100; i++ )
-  { Div3 = (i%3);
-    Div5 = (i%5);
-     
-
+ {
+  
+  for (j=2; j<i; j++) { 
+   
+    if ((i%i==0)&&(i%1==0)&&(i%j==0))
+    {
     
-   
-    if ((Div3==0) && (Div5==0))
-      console.log(i + " Fizz +Buzz")
+    currentNumberIsPrime = false;
+    break;
+
+    }
+  
+ }
+ if (currentNumberIsPrime == true) {
+  console.log("Next prime number is " + i);
+  break;
+}
+
+else {
+  currentNumberIsPrime = true;
+}
+}
 
 
-      else      {     if (Div5 ==0){
-                      console.log(i + " Buzz")}
-                      else { if (Div3 == 0 )
-                        console.log(i + " Fizz ")}
-       
-       }
-                
-  }  
-   
-
-
-
-
-
+        
